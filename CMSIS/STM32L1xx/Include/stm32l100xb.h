9abb3395be5d7988ed26a721517de0edbcc493de
Jolatomme <47304809+Jolatomme@users.noreply.github.com>
2019-02-05 00:34:35 +0100

    Upload HAL for L152

---
+++ CMSIS/STM32L1xx/Include/stm32l100xb.h
@@ -0,0 +1,12 @@
+/**
+  ******************************************************************************
+  * @file    stm32l100xb.h
+  * @author  MCD Application Team
+  * @brief   CMSIS Cortex-M3 Device Peripheral Access Layer Header File. 
+  *          This file contains all the peripheral register's definitions, bits 
+  *          definitions and memory mapping for STM32L1xx devices.            
+  *            
+  *          This file contains:
+  *           - Data structures and the address mapping for all peripherals
+  *           - Peripheral's registers declarations and bits definition
+  *           - Macros to access peripheral
